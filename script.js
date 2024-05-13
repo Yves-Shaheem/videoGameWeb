5bd05825a5e4bf7207915e38617b11378e698b6f
Yves-Shaheem Shedid <2233669@collegemv.qc.ca>
2024-05-13 17:42:35 -0400

    box position fixed

--- script.js
+++ script.js
@@ -156,17 +156,18 @@ function onKeyDown(key) {
     // Up arrow is 87
     if (key.keyCode === 87 || key.keyCode === 38) {
         // If the W key or the Up arrow is pressed, move the player up.
-        if (playerTankSprite.position.y != 0) {
+        if (playerTankSprite.position.y > 0) {
             // Don't move up if the player is at the top of the stage
             playerTankSprite.position.y -= boxHeight;
         }
+        
     }
 
     // S Key is 83
     // Down arrow is 40
     if (key.keyCode === 83 || key.keyCode === 40) {
         // If the S key or the Down arrow is pressed, move the player down.
-        if (playerTankSprite.position.y != resolutionY - boxHeight) {
+        if (playerTankSprite.position.y < 750) {
             // Don't move down if the player is at the bottom of the stage
             playerTankSprite.position.y += boxHeight;
         }
@@ -176,7 +177,7 @@ function onKeyDown(key) {
     // Left arrow is 37
     if (key.keyCode === 65 || key.keyCode === 37) {
         // If the A key or the Left arrow is pressed, move the player to the left.
-        if (playerTankSprite.position.x != 0) {
+        if (playerTankSprite.position.x > 0) {
             // Don't move to the left if the player is at the left side of the stage
             playerTankSprite.position.x -= boxWidth;
         }
@@ -186,7 +187,7 @@ function onKeyDown(key) {
     // Right arrow is 39
     if (key.keyCode === 68 || key.keyCode === 39) {
         // If the D key or the Right arrow is pressed, move the player to the right.
-        if (playerTankSprite.position.x != resolutionX - boxWidth) {
+        if (playerTankSprite.position.x < 750) {
             // Don't move to the right if the player is at the right side of the stage
             playerTankSprite.position.x += boxWidth;
         }
